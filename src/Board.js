73e0c074720c2204cac631d44006f17d92b46eca
burningmagic <burningmagic@nexon.co.kr>
2020-02-03 19:19:06 +0900

    보드 페이지 그리는 함수 분리

--- src/Board.js
+++ src/Board.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import List from './List'; 
 import {useParams } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-function Topic() {
-    let { id } = useParams();
-    return <h3>Requested topic ID: {id}</h3>;
-}
 
 class Board extends React.Component {
+
+    renderTopic() {
+        let { id } = useParams();
+        return <h3>{id} 게시판</h3>;
+    }
+
+    renderBoardList() {
+        return <div>test</div>
+    }
+
+    renderMakeNewBoard() {
+        return (
+            <div>
+                <ListGroup horizontal>
+                    <Form.Control type="text" placeholder="Enter List Name" />
+                    <Form.Control type="text" placeholder="Enter List Name" />
+                    <Form.Control type="text" placeholder="Enter List Name" />
+                </ListGroup>
+            </div>
+        );
+    }
+
     render(){
         return (
             <div>
-                <Topic />
+                <this.renderTopic />
+                <this.renderBoardList />
+                <this.renderMakeNewBoard />
             </div>
         );
     }
