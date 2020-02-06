3666cf38dc31aaa9095d5643fc5dca4a8d88147e
burningmagic <burningmagic@nexon.co.kr>
2020-02-06 19:55:48 +0900

    making lane

--- src/Board.js
+++ src/Board.js
@@ -7,33 +7,58 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 class Board extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.setState({
+            laneName: "",
+        });
+        this.laneList = [];
+    }
+
     renderTopic() {
         let { id } = useParams();
         return <h3>{id} 게시판</h3>;
     }
 
-    renderBoardList() {
-        return <div>test</div>
-    }
+    setName = (value) => {
+        this.setState({
+            laneName: value,
+        });
+    };
 
-    renderMakeNewBoard() {
-        return (
-            <div>
-                <ListGroup horizontal>
-                    <Form.Control type="text" placeholder="Enter List Name" />
-                    <Form.Control type="text" placeholder="Enter List Name" />
-                    <Form.Control type="text" placeholder="Enter List Name" />
-                </ListGroup>
-            </div>
-        );
-    }
+    _handleKeyDown = (e, callback) => {
+      if (e.key === 'Enter') {
+        callback();
+      }
+    };
+
+    _onChange = (e, callback) => {
+        callback();
+    };
+
+    renderLaneList = () => {
+        return this.laneList.map(laneName => {
+            return (                
+                <div className="board-default boardMakeItem"> 
+                    {laneName}
+                </div>
+            );
+        });
+    } 
+
 
     render(){
         return (
             <div>
                 <this.renderTopic />
-                <this.renderBoardList />
-                <this.renderMakeNewBoard />
+                <ListGroup horizontal>
+                    <div>
+                        {this.renderLaneList()}
+                    </div>
+                    <div className="board-default boardMakeItem">
+                        <Form.Control type="text" placeholder="Enter List Name" onChange={e => this.setName(e, () => this.setState({laneName:e.target.value}))} onKeyDown={e => this._handleKeyDown(e, () => this.laneList.push(e.target.value))} />
+                    </div>
+                </ListGroup>
             </div>
         );
     }
